692653e148d2f49359af4d18d749f4b548dc0540
Siddhesh Mangela <sidthecool007@gmail.com>
2025-01-21 14:20:31 +0530

    Silk update added

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import GlobalContainer from '$lib/components/GlobalContainer';
+  import type { SvelteComponentProps } from '$lib/types/svelte-component';
+
+  const { children }: SvelteComponentProps = $props();
+</script>
+
+<GlobalContainer />
+
+<div>
+  {#if children}
+    {@render children()}
+  {/if}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,31 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  import Box from '$lib/components/Box.svelte';
+  import SilkUpdateTool from '$lib/components/SilkUpdateTool.svelte';
+  import Stack from '$lib/components/ui-framework/Layout/Stack/Stack.svelte';
+  import StackItem from '$lib/components/ui-framework/Layout/Stack/StackItem.svelte';
+  import { useCountStore } from '$lib/stores/count/count.svelte';
+</script>
+
+<main>
+  <Box>
+    <Stack space={2}>
+      <StackItem><h1><b>{useCountStore.count}</b> Silks Pending</h1></StackItem>
+      <StackItem><SilkUpdateTool /></StackItem>
+    </Stack>
+  </Box>
+</main>
+
+<style lang="scss">
+  main {
+    height: 100vh;
+    width: 100vw;
+    overflow-y: auto;
+    overflow-x: hidden;
+  }
+
+  h1 {
+    font-weight: 500;
+    text-align: center;
+    font-size: 2rem;
+  }
+</style>
